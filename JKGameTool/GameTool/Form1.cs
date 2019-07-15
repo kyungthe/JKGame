17e1d52ab11191f2c4240986c0d66bb14267617b
QooJuice <myungthe@naver.com>
2019-07-15 18:06:48 +0900

    1. Grid 구현 2. 사각형 그리기 구현 3. 더블 버퍼링 사용

---
+++ JKGameTool/GameTool/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameTool
+{
+    public partial class Form1 : Form
+    {
+        // 점선의 점 간격
+        private float dotInterval = 2.0f;
+        // 격자 간격(Point 단위)
+        private float gridInterval;
+        private float pixelPerPoint;
+        private int point = 20;
+        private Pen pen;
+        private State currentState;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializePaintValues();
+            currentState = new CreateUINoneState(this);
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            SetStyle(ControlStyles.UserPaint, true);
+        }
+
+        private void InitializePaintValues()
+        {
+            Graphics graphics = Graphics.FromHwnd(IntPtr.Zero);
+            Color color = ForeColor;
+            pen = new Pen(color);
+
+            pen.Width = dotInterval;
+
+            // Point : Pixel = 72 : DPI
+            // Point를 알고 있을 때 Pixel = (Point * DPI) / 72
+            float dpi = graphics.DpiX;
+            pixelPerPoint = dpi / 72;
+            gridInterval = pixelPerPoint * point;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            DrawGrid(e.Graphics);
+            currentState.Draw(e.Graphics);
+        }
+
+        private void ChangeState(State state)
+        {
+            currentState.Exit();
+
+            currentState = state;
+
+            currentState.Enter();
+        }
+
+        private void CreateButtonButton_Click(object sender, EventArgs e)
+        {
+            ChangeState(new CreateUIButtonState(this));
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+            case Keys.Escape:
+                    ChangeState(new CreateUINoneState(this));
+                    break;
+            default:
+                    break;
+            }
+        }
+
+        public void DrawGrid(Graphics graphics)
+        {
+            float startX = Width - ClientSize.Width;
+            float startY = Height - ClientSize.Height;
+            float width = CreateButtonButton.Location.X - 10;
+            float height = ClientSize.Height;
+
+            pen.DashStyle = System.Drawing.Drawing2D.DashStyle.Dot;
+
+            //세로 그리기
+            for (float x = startX / gridInterval; x < width; x += gridInterval)
+            {
+                graphics.DrawLine(pen, x, 0, x, height);
+            }
+
+            //가로 그리기
+            for (float y = startY / gridInterval; y < height; y += gridInterval)
+            {
+                graphics.DrawLine(pen, 0, y, width, y);
+            }
+        }
+
+        public void DrawButton(Graphics graphics, int startX, int startY, int endX, int endY)
+        {
+            pen.DashStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+
+            int width = endX - startX;
+            int height = endY - startY;
+
+            if ((width <= 0) || (height <= 0))
+            {
+                return;
+            }
+            
+            graphics.DrawRectangle(pen, new Rectangle(startX, startY, width, height));
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            currentState.MouseDown(e.X, e.Y);
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            currentState.MouseMove(e.X, e.Y);
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            currentState.MouseUp();
+        }
+    }
+}
---
+++ JKGameTool/GameTool/State.cs
@@ -0,0 +1,89 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace GameTool
+{
+    abstract class State
+    {
+        protected Form1 parentForm = null;
+
+        protected State(Form1 parentForm)
+        {
+            this.parentForm = parentForm;
+        }
+
+        public abstract void Enter();
+        public abstract void MouseDown(int x, int y);
+        public abstract void MouseMove(int x, int y);
+        public abstract void MouseUp();
+        public abstract void Draw(Graphics graphics);
+        public abstract void Exit();
+    }
+
+    class CreateUINoneState : State
+    {
+        public CreateUINoneState(Form1 parentForm) : base(parentForm)
+        {
+        }
+
+        public override void Enter()
+        {
+            parentForm.Cursor = Cursors.Default;
+        }
+        public override void MouseDown(int x, int y) { }
+        public override void MouseMove(int x, int y) { }
+        public override void MouseUp() { }
+        public override void Draw(Graphics graphics) { }
+        public override void Exit() { }
+    }
+
+    class CreateUIButtonState : State
+    {
+        private int startX = 0;
+        private int startY = 0;
+        private int endX = 0;
+        private int endY = 0;
+        private bool isClicked = false;
+
+        public CreateUIButtonState(Form1 parentForm) : base(parentForm)
+        {
+        }
+
+        public override void Enter()
+        {
+            parentForm.Cursor = Cursors.Cross;
+        }
+
+        public override void MouseDown(int x, int y)
+        {
+            startX = x;
+            startY = y;
+            isClicked = true;
+        }
+
+        public override void MouseMove(int x, int y)
+        {
+            if(isClicked)
+            {
+                endX = x;
+                endY = y;
+                parentForm.Invalidate();
+            }
+        }
+
+        public override void MouseUp()
+        {
+            isClicked = false;
+        }
+
+        public override void Draw(Graphics graphics)
+        {
+            if (isClicked)
+            {
+                parentForm.DrawButton(graphics, startX, startY, endX, endY);
+            }
+        }
+
+        public override void Exit() { }
+    }
+}
